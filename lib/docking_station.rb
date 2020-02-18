0a097f855b3040e52496d052d23abeffda964735
Patrick Oliver <poliver24@hotmail.com>
2020-02-18 17:30:05 +0000

    step11 commit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,23 @@
 class Dockingstation
+
   def release_bike
     Bike.new
+  end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike
+    @bike
   end
 end
 
 class Bike
   def working?
+    true
+  end
+  def docked?
     true
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,16 @@
 require 'docking_station'
 
 describe Dockingstation do
-  # subject(:) { described_class.new}
   it { is_expected.to respond_to :release_bike}
 
   it 'releases working bike' do
     bike = subject.release_bike
     expect(bike).to be_working
+  end
+
+  it 'docks a bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end

b9d189de48c299273a7725bb9487af0c1e3a5ced
Patrick Oliver <poliver24@hotmail.com>
2020-02-18 16:43:27 +0000

    Added working bikes and tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require 'docking_station'
 
 describe Dockingstation do
+  # subject(:) { described_class.new}
   it { is_expected.to respond_to :release_bike}
+
+  it 'releases working bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
